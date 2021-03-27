dbcaa3a8446981893aff4c0368054db153ec38f9
adamek195 <248955@student.pwr.edu.pl>
2021-03-27 01:50:07 +0100

    remove comments

--- CovidStatisticsApp/CovidStatisticsApp/Views/WindowLogin.xaml.cs
+++ CovidStatisticsApp/CovidStatisticsApp/Views/WindowLogin.xaml.cs
@@ -14,25 +14,16 @@ using System.Windows.Shapes;
 
 namespace CovidStatisticsApp.Views
 {
-    /// <summary>
-    /// Interaction logic for WindowLogin.xaml
-    /// </summary>
     public partial class WindowLogin : Window
     {
         public WindowLogin()
         {
             InitializeComponent();
-            //Set colors of backgrounds in Login Window
             ButtonSignIn.Background = Brushes.IndianRed;
             ButtonChangePassword.Background = Brushes.IndianRed;
             ButtonSignUp.Background = Brushes.IndianRed;
         }
 
-        /// <summary>
-        /// Sign in button check password and personal data
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ButtonSignIn_Click(object sender, RoutedEventArgs e)
         {
 
