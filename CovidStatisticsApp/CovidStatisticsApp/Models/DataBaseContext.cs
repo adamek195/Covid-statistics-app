317b221497a769487606f01839cec1eddd1fce31
adamek195 <248955@student.pwr.edu.pl>
2021-03-27 02:01:03 +0100

    remove comments

--- CovidStatisticsApp/CovidStatisticsApp/Models/DataBaseContext.cs
+++ CovidStatisticsApp/CovidStatisticsApp/Models/DataBaseContext.cs
@@ -3,18 +3,12 @@ using System.Data.Entity;
 
 namespace CovidStatisticsApp.Models
 {
-    /// <summary>
-    /// Class where is initalized context of database
-    /// </summary>
     public class DataBaseContext: DbContext
     {
         public DataBaseContext() : base("ConnectionString")
         {
         }
 
-        /// <summary>
-        /// Initialize tables in a database
-        /// </summary>
         public DbSet<User> Users { get; set; }
     }
 }
