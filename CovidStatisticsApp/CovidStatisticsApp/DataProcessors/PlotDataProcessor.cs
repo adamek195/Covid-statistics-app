9f86de894c4e48edb7d2afd35e22ba8fca74e62e
Kajetan <kajetanzdanowicz@wp.pl>
2021-04-17 18:01:31 +0200

    code cleaned

--- CovidStatisticsApp/CovidStatisticsApp/DataProcessors/PlotDataProcessor.cs
+++ CovidStatisticsApp/CovidStatisticsApp/DataProcessors/PlotDataProcessor.cs
@@ -26,101 +26,19 @@ namespace CovidStatisticsApp.DataProcessors
 
     public class PlotDataProcessor
     {
-        private readonly List<CovidStatisticsDataViewModel> WeekList;
-        private readonly List<CovidStatisticsDataViewModel> MonthList;
-        private readonly List<CovidStatisticsDataViewModel> HalfYearList;
         private readonly List<CovidStatisticsDataViewModel> OverallList;
 
         public PlotDataProcessor(List<CovidStatisticsDataViewModel> DataList)
         {
-            this.WeekList = DataList.GetRange(DataList.Count - 14, 14);
-            this.MonthList = DataList.GetRange(DataList.Count - 30, 30);
-            this.HalfYearList = DataList.GetRange(DataList.Count - 180, 180);
             this.OverallList = DataList;
         }
 
         public List<int> ReturnCasesInGivenPeriodAndType(Period period, CaseType caseType)
         {
-            if (caseType == CaseType.Death)
-            {
-                if (period == Period.TwoWeeks)
-                {
-                    return this.WeekList.Select(element => element.DeathCases).ToList();
-                }
-                if (period == Period.Month)
-                {
-                    return this.MonthList.Select(element => element.DeathCases).ToList();
-                }
-                if (period == Period.HalfYear)
-                {
-                    return this.HalfYearList.Select(element => element.DeathCases).ToList();
-                }
-                if (period == Period.Overall)
-                {
-                    return this.OverallList.Select(element => element.DeathCases).ToList();
-                }
-            }
-
-            if (caseType == CaseType.Recovered)
-            {
-                if (period == Period.TwoWeeks)
-                {
-                    return this.WeekList.Select(element => element.RecoveredCases).ToList();
-                }
-                if (period == Period.Month)
-                {
-                    return this.MonthList.Select(element => element.RecoveredCases).ToList();
-                }
-                if (period == Period.HalfYear)
-                {
-                    return this.HalfYearList.Select(element => element.RecoveredCases).ToList();
-                }
-                if (period == Period.Overall)
-                {
-                    return this.OverallList.Select(element => element.RecoveredCases).ToList();
-                }
-            }
-
-            if (caseType == CaseType.Active)
-            {
-                if (period == Period.TwoWeeks)
-                {
-                    return this.WeekList.Select(element => element.ActiveCases).ToList();
-                }
-                if (period == Period.Month)
-                {
-                    return this.MonthList.Select(element => element.ActiveCases).ToList();
-                }
-                if (period == Period.HalfYear)
-                {
-                    return this.HalfYearList.Select(element => element.ActiveCases).ToList();
-                }
-                if (period == Period.Overall)
-                {
-                    return this.OverallList.Select(element => element.ActiveCases).ToList();
-                }
-            }
-
-            if (caseType == CaseType.Confirmed)
-            {
-                if (period == Period.TwoWeeks)
-                {
-                    return this.WeekList.Select(element => element.ConfirmedCases).ToList();
-                }
-                if (period == Period.Month)
-                {
-                    return this.MonthList.Select(element => element.ConfirmedCases).ToList();
-                }
-                if (period == Period.HalfYear)
-                {
-                    return this.HalfYearList.Select(element => element.ConfirmedCases).ToList();
-                }
-                if (period == Period.Overall)
-                {
-                    return this.OverallList.Select(element => element.ConfirmedCases).ToList();
-                }
-            }
-            return this.OverallList.Select(element => element.ConfirmedCases).ToList();
+            var caseTypeHelper = new CaseTypeHelper(this.OverallList);
+            var listWithSpecifiedType = caseTypeHelper.GetSpecificCases(caseType);
+            var periodHelper = new PeriodHelper(listWithSpecifiedType);
+            return periodHelper.GetSpecificPeriod(period);
         }
     }
 }
