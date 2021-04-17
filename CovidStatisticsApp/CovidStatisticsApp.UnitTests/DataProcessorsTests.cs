5daf52a187527e4b1a088bf511f5400792d295a8
Kajetan <kajetanzdanowicz@wp.pl>
2021-04-17 22:07:55 +0200

    period helper tests finished

--- CovidStatisticsApp/CovidStatisticsApp.UnitTests/DataProcessorsTests.cs
+++ CovidStatisticsApp/CovidStatisticsApp.UnitTests/DataProcessorsTests.cs
@@ -10,13 +10,15 @@ namespace CovidStatisticsApp.UnitTests
     public class CaseTypeHelperTests
     {
         List<int> randomCasesList;
+        int OverallDays;
 
         [TestInitialize]
         public void TestInitialize()
         {
             var generator = new Random();
             var randomCasesList = new List<int>();
-            for (int i = 0; i < 360; i++)
+            this.OverallDays = generator.Next(minValue: 200, maxValue: 500);
+            for (int i = 0; i < this.OverallDays; i++)
             {
                 randomCasesList.Add(generator.Next(minValue: 20, maxValue: 20000));
             }
@@ -28,6 +30,27 @@ namespace CovidStatisticsApp.UnitTests
         {
             var periodHelper = new PeriodHelper(this.randomCasesList);
             Assert.AreEqual<int>(periodHelper.GetSpecificPeriod(Period.TwoWeeks).Count, 14);
+        }
+
+        [TestMethod]
+        public void Test_CaseTypeHelper_ReturnsMonthAs30Days()
+        {
+            var periodHelper = new PeriodHelper(this.randomCasesList);
+            Assert.AreEqual<int>(periodHelper.GetSpecificPeriod(Period.Month).Count, 30);
+        }
+
+        [TestMethod]
+        public void Test_CaseTypeHelper_ReturnsHalfYearAs180Days()
+        {
+            var periodHelper = new PeriodHelper(this.randomCasesList);
+            Assert.AreEqual<int>(periodHelper.GetSpecificPeriod(Period.HalfYear).Count, 180);
+        }
+
+        [TestMethod]
+        public void Test_CaseTypeHelper_ReturnsOverallAsOverallDays()
+        {
+            var periodHelper = new PeriodHelper(this.randomCasesList);
+            Assert.AreEqual<int>(periodHelper.GetSpecificPeriod(Period.Overall).Count, this.OverallDays);
         }
     }
 }
