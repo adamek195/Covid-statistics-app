d571ca66ecc4f2e58928b6e4e91202c998dd46ac
Kajetan <kajetanzdanowicz@wp.pl>
2021-04-24 17:56:27 +0200

    daily data processor, overloaded - operator for covid statistics data view model

---
+++ CovidStatisticsApp/CovidStatisticsApp/DataProcessors/DailyCasesProcessor.cs
@@ -0,0 +1,39 @@
+﻿using CovidStatisticsApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidStatisticsApp.DataProcessors
+{
+    public class DailyCasesProcessor
+    {
+        private readonly List<CovidStatisticsDataViewModel> DataList;
+
+        public DailyCasesProcessor(List<CovidStatisticsDataViewModel> dataList)
+        {
+            this.DataList = dataList;
+        }
+
+        public List<CovidStatisticsDataViewModel> CalculateDailyCases()
+        {
+            var DailyCasesList = new List<CovidStatisticsDataViewModel>
+            {
+                new CovidStatisticsDataViewModel
+                {
+                    DeathCases = 0,
+                    ActiveCases = 0,
+                    ConfirmedCases = 0,
+                    RecoveredCases = 0
+                }
+            };
+            for (int i = 0; i < this.DataList.Count - 1; i++)
+            {
+                DailyCasesList.Add(this.DataList[i] - this.DataList[i + 1]);
+            }
+            return DailyCasesList;
+        }
+
+    }
+}
